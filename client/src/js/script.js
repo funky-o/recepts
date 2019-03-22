c243792aea7f6fa7359445931e13e7365ec2cac0
elivanov.denis <elivanov.denis@mediasoft.team>
2019-03-22 22:06:19 +0400

    change

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -28,75 +28,67 @@ import $ from "jquery";
   
   var showItem = urlParams.get('id');
 
-  if (showItems) {
-    $.ajax({
-      type: 'GET',
-      url: `${webLink}/api/data/getRecepts?find=${showItems}`,
-      async: false,
-      success: gererateRecepts,
-      error: function(error){
-        console.log("Ошибка", error);
-      }
-    });
-  }
-
   if (showItem) {
     $.ajax({
       type: 'GET',
       url: `${webLink}/api/data/getRecept?find=${showItems}&id=${showItem}`,
       async: false,
-      success: gererateRecept,
+      success: function(element) {
+        console.log(element);
+        var recept = element;
+        console.log(recept);
+        $('.item-img').attr('src', `images/${showItems}/${recept.src}`);
+        $('.item-name').text(recept.name);
+        $('.item-description').text(recept.description);
+    
+        var needItemsItem = $('.needItems-item').detach();
+        var needForDoItem = $('.needForDo-item').detach();
+    
+        recept['needItems'].forEach(function(text) {
+          var newItem = needItemsItem.clone();
+          newItem.text(text);
+          $('.needItems').append(newItem);
+        });
+    
+        recept['needForDo'].forEach(function(text, i) {
+          var newItem = needForDoItem.clone();
+          newItem.html(`<strong>${i+1}</strong>: ${text}`);
+        $('.needForDo').append(newItem);
+        });
+        
+        
+      },
+      error: function(error){
+        console.log("Ошибка", error);
+      }
+    });
+  } else if (showItems) {
+    $.ajax({
+      type: 'GET',
+      url: `${webLink}/api/data/getRecepts?find=${showItems}`,
+      async: false,
+      success: function(data) {
+        console.log(data);
+        var arrayRecepts = data;
+        var block = $('.js-keep');
+        var template = $('.js-save').detach();
+        arrayRecepts.forEach(function(element) {
+          var cloneTemplate = template.clone();
+          cloneTemplate.attr('data-id', element.id);
+          cloneTemplate.attr('data-recepts', showItems);
+          cloneTemplate.find('.item-img').attr('src', `images/${showItems}/${element.src}`);
+          cloneTemplate.find('.item-name').text(element.name);
+          cloneTemplate.find('.item-description').text(element.description);
+          block.append(cloneTemplate);
+        });
+      },
       error: function(error){
         console.log("Ошибка", error);
       }
     });
   }
 
-
-  function gererateRecepts (data) {
-    console.log(data);
-    var arrayRecepts = data;
-    var block = $('.js-keep');
-    var template = $('.js-save').detach();
-    arrayRecepts.forEach(function(element) {
-      var cloneTemplate = template.clone();
-      cloneTemplate.attr('data-id', element.id);
-      cloneTemplate.attr('data-recepts', showItems);
-      cloneTemplate.find('.item-img').attr('src', `images/${showItems}/${element.src}`);
-      cloneTemplate.find('.item-name').text(element.name);
-      cloneTemplate.find('.item-description').text(element.description);
-      block.append(cloneTemplate);
-    });
-    
-  }
-
-  function gererateRecept (element) {
-    console.log(element);
-    var recept = element;
-    console.log(recept);
-    $('.item-img').attr('src', `images/${showItems}/${recept.src}`);
-    $('.item-name').text(recept.name);
-    $('.item-description').text(recept.description);
-
-    var needItemsItem = $('.needItems-item').detach();
-    var needForDoItem = $('.needForDo-item').detach();
-
-    recept['needItems'].forEach(function(text) {
-      var newItem = needItemsItem.clone();
-      newItem.text(text);
-      $('.needItems').append(newItem);
-    });
-
-    recept['needForDo'].forEach(function(text, i) {
-      var newItem = needForDoItem.clone();
-      newItem.html(`<strong>${i+1}</strong>: ${text}`);
-    $('.needForDo').append(newItem);
-    });
-    
-    
-
-  }
-
+  
   $(document).on('click', '.js-save', function() {
     var id = $(this).attr('data-id');
     window.location.href = `recept.html?show-recepts=${showItems}&id=${id}`;    
